3b658c915d7f1160018768f8916201c7377c212f
hoanganh121199 <anhvmh.vnist@gmail.com>
2021-06-10 14:37:43 +0700

    update sql

--- server/jlpt.sql
+++ server/jlpt.sql
@@ -4,6 +4,12 @@ USE jlpt;
 -- Table structure for table `grammarpractice`
 --
 
+-- MySQL dump 10.13  Distrib 8.0.20, for Win64 (x86_64)
+--
+-- Host: localhost    Database: jlpt
+-- ------------------------------------------------------
+-- Server version	8.0.20
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
@@ -107,7 +113,7 @@ CREATE TABLE `questionpracticevocabulary` (
   `result` varchar(1000) NOT NULL,
   `idRLG` int NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -116,7 +122,7 @@ CREATE TABLE `questionpracticevocabulary` (
 
 LOCK TABLES `questionpracticevocabulary` WRITE;
 /*!40000 ALTER TABLE `questionpracticevocabulary` DISABLE KEYS */;
-INSERT INTO `questionpracticevocabulary` VALUES (1,'きのう　かいしゃを　*やすみました*','友みました','休みました','安みました','体みました','1',2),(2,'いすの*した*に　かばんが　あります。','才','丅','下','干','3',2),(3,'けさ　おそく　うちを　*出ました*','だしました','でました','でりました','きました','2',2),(4,'*せんせい*は　いつも　いそがしいです。','先生','生先','先牛','矢王','1',2),(5,'けさ　おそく　うちを　*出ました*','だしました','でました','でりました','きました','2',2),(6,'アンさんは　ぼうしを　（　　）　います。','きて','はいて','かけて','かぶって','4',2),(7,'（　　）　のみものが　ほしいです　。','いたい','つめたい','あかるい','さむい','2',2),(8,'ふくを　（　　）、　おふろに　はいります。','すって','すわって','けして','ぬいで','4',2),(9,'この　かばんは　*高い*です','たかい','だかい','たがい','だがい','1',2),(10,'*わたしは　はじめて　にほんに　きました*。','わたしは　にほんに　きたことが　ありませんでした。','わたしは　ことし　にほんに　きました。','わたしは　らいねん　にほんに　きます。','わたしは　にほんに　きたかったです。','1',2),(11,'*わたしは　フランスごを　ならっています*。','わたしは　フランスごを　おしえて　います。','わたしは　フランスごを　かいて　います。','わたしは　フランスごを　べんきょうして　います。','わたしは　フランスごを　よんで　います。','3',2),(12,'*たなかさんは　もう　かえりました*。','たなかさんは　きのう　きました。','たなかさんは　どこも　いません。','たなかさんは　ここに　いません。','たなかさんは　きょう　やすみでした。','1',2);
+INSERT INTO `questionpracticevocabulary` VALUES (1,'きのう　かいしゃを　*やすみました*','友みました','休みました','安みました','体みました','3',2),(2,'いすの*した*に　かばんが　あります。','才','丅','下','干','3',2),(3,'けさ　おそく　うちを　*出ました*','だしました','でました','でりました','きました','2',2),(4,'*せんせい*は　いつも　いそがしいです。','先生','生先','先牛','矢王','1',2),(5,'けさ　おそく　うちを　*出ました*','だしました','でました','でりました','きました','2',2),(6,'アンさんは　ぼうしを　（　　）　います。','きて','はいて','かけて','かぶって','4',2),(7,'（　　）　のみものが　ほしいです　。','いたい','つめたい','あかるい','さむい','2',2),(8,'ふくを　（　　）、　おふろに　はいります。','すって','すわって','けして','ぬいで','4',2),(9,'この　かばんは　*高い*です','たかい','だかい','たがい','だがい','1',2),(10,'*わたしは　はじめて　にほんに　きました*。','わたしは　にほんに　きたことが　ありませんでした。','わたしは　ことし　にほんに　きました。','わたしは　らいねん　にほんに　きます。','わたしは　にほんに　きたかったです。','1',2),(11,'*わたしは　フランスごを　ならっています*。','わたしは　フランスごを　おしえて　います。','わたしは　フランスごを　かいて　います。','わたしは　フランスごを　べんきょうして　います。','わたしは　フランスごを　よんで　います。','3',2),(12,'*たなかさんは　もう　かえりました*。','たなかさんは　きのう　きました。','たなかさんは　どこも　いません。','たなかさんは　ここに　いません。','たなかさんは　きょう　やすみでした。','1',2),(13,'まいあさ、ジョギングをして、シャワーを　（　　　）。','はいります','いれます','あびます','よびます','3',2),(14,'いままで　（　　　）にほんごを　べんきょうしましたか。','いくつ','いくら','どのくらい','どちら','3',2);
 /*!40000 ALTER TABLE `questionpracticevocabulary` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -134,7 +140,7 @@ CREATE TABLE `users` (
   `password` varchar(40) NOT NULL,
   `admin` int NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -143,7 +149,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'hoanganh','vuminhhoanganh12@gmail.com','4297f44b13955235245b2497399d7a93',0),(2,'tra123','nvd.vnist@gmail.com','cfac4e0dc47d98414cf373a2f09d28a1',0),(3,'hoanganh123','vu@gmail.com','e034fb6b66aacc1d48f445ddfb08da98',0),(4,'ha','admin.vnist@gmail.com','4297f44b13955235245b2497399d7a93',0),(5,'da','nva.vnist@gmail.com','4297f44b13955235245b2497399d7a93',0),(6,'1212','12@gmail.com','202cb962ac59075b964b07152d234b70',0),(7,'admin','admin@gmail.com','21232f297a57a5a743894a0e4a801fc3',1);
+INSERT INTO `users` VALUES (1,'hoanganh','vuminhhoanganh12@gmail.com','4297f44b13955235245b2497399d7a93',0),(7,'admin','admin@gmail.com','21232f297a57a5a743894a0e4a801fc3',1),(9,'tra','nvd.vnist@gmail.com','4297f44b13955235245b2497399d7a93',0),(10,'ha','nva.vnist@gmail.com','4297f44b13955235245b2497399d7a93',0),(11,'da','super.admin.vnist@gmail.com','4297f44b13955235245b2497399d7a93',0),(12,'Vũ Hoàng Anh','admin.vnist@gmail.com','202cb962ac59075b964b07152d234b70',0);
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -167,7 +173,7 @@ CREATE TABLE `value` (
 
 LOCK TABLES `value` WRITE;
 /*!40000 ALTER TABLE `value` DISABLE KEYS */;
-INSERT INTO `value` VALUES (1,2,2);
+INSERT INTO `value` VALUES (1,2,2),(2,2,3),(3,2,3),(4,2,1),(5,2,7),(6,2,5),(9,2,11),(10,2,4),(11,2,4),(12,2,5);
 /*!40000 ALTER TABLE `value` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -204,4 +210,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-06-04  9:32:14
+-- Dump completed on 2021-06-10 14:36:27
